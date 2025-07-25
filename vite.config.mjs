d7c9ecb48cbe8e79e941cfa16fd18f9b6d540077
cobrianz <briancheruiyot.h@gmail.com>
2025-07-25 22:49:46 +0300

    first comiit

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,10 +7,10 @@ import tagger from "@dhiwise/component-tagger";
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
-  build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
-  },
+build: {
+  outDir: "build/dist",
+  chunkSizeWarningLimit: 2000,
+},
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
     port: "4028",
